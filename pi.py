4bf0c8facdb615a57f47364a1d9894b0b816cdba
Suzuki Daiki <achillesbigtree@gmail.com>
2024-04-02 16:01:12 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -4,4 +4,5 @@ text = """
 """
 
 # TODO
+list(map(len["How","I","want","a","drink","alcoholic","of","course","after","the","heavy","chapters","involving","quantum","mechanics","All","of","the","geometry","Herr","Planck","is","fairly","hard"]))
 
