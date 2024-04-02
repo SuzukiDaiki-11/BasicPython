2145882eb4eb4ae907ed1a382565bfcca630075a
Suzuki Daiki <achillesbigtree@gmail.com>
2024-04-02 16:01:12 +0900

    Python入門の解答

--- pi.py
+++ pi.py
@@ -1,7 +1,24 @@
-text = """
+txt = """
     How I want a drink, alcoholic of course, after the heavy chapters involving
     quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.
 """
 
 # TODO
+print("""How I want a drink, alcoholic of course, after the heavy chapters involving
+    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard""".split())
+replace = (','<'')
+
+print = (txt.strip())
+
+list(map(len,["txt"]))[" "]
+print (txt.count)
+
+py = list(map(len,txt.replace('.','').replace(',', '').split()))
+print(py)
+answer = ""
+for i in py:
+    answer += str(i)
+
+print(answer)
+
 
