2cc41e3ab0da23b691a750bc2b801713a0b9587c
Suzuki Daiki <achillesbigtree@gmail.com>
2024-04-03 14:39:07 +0900

    (修正)Python入門の解答

--- pi.py
+++ pi.py
@@ -5,13 +5,7 @@ txt = """
 
 # TODO
 print("""How I want a drink, alcoholic of course, after the heavy chapters involving
-    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard""".split())
-replace = (','<'')
-
-print = (txt.strip())
-
-list(map(len,["txt"]))[" "]
-print (txt.count)
+    quantum mechanics. All of thy geometry, Herr Planck, is fairly hard.""")
 
 py = list(map(len,txt.replace('.','').replace(',', '').split()))
 print(py)
@@ -20,5 +14,3 @@ for i in py:
     answer += str(i)
 
 print(answer)
-
-
